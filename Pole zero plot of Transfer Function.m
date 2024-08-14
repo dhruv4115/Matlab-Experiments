1a59948bb92929bccf5d01694c4d5f62fb2abb7e
DHRUV KUMAR TIWARI <129687870+dhruv4115@users.noreply.github.com>
2024-08-14 14:59:10 +0530

    Pole zero plot for a given transfer function

---
+++ Pole zero plot of Transfer Function.m
@@ -0,0 +1,26 @@
+%define the numerator coefficients of transfer function
+num = [5 -9 16 -14];
+
+%define the denominator coefficients of transfer function
+den = [1 -2 10 -4 64];
+
+%create the transfer function
+sys = tf(num, den);
+
+%plot the pole-zero plot map
+figure;
+
+pzmap(sys);
+hold on;
+poles = pole(sys);
+zeros = zero(sys);
+
+%plot poles
+plot(real(poles),imag(poles), 'r*','MarkerSize',8,'DisplayName','Poles');
+
+%plot zeros
+plot(real(poles),imag(poles), 'bo','MarkerSize',8,'DisplayName','zeros');
+
+title('Pole-Zero Plot ___20224058');
+legend('Poles','Zeros');
+grid on;
